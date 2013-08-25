d92eb53a1627b999e9ed997a5e97cccf2a8ff6d6
Vladimir Darmin <vladimir.darmin@gmail.com>
2013-08-24 22:27:17 -0700

    forgot to include new files

---
+++ config.js
@@ -0,0 +1,14 @@
+exports.SOFTWARE = 'vs-stun';
+
+exports.BLOCKED_UDP = 'Blocked UDP';
+exports.OPEN_INTERNET = 'Open Internet';
+
+exports.SYMMETRIC_FIREWALL = 'Symmetric Firewall';
+
+exports.FULL_CONE_NAT = 'Full Cone NAT';
+exports.SYMMETRIC_NAT = 'Symmetric NAT';
+exports.RESTRICTED_CONE_NAT = 'Restricted Cone NAT';
+exports.PORT_RESTRICTED_CONE_NAT = 'Port Restricted Cone NAT';
+
+exports.RETRANSMISSION_COUNT = 7;
+exports.RETRANSMISSION_TIMEOUT = 500;
